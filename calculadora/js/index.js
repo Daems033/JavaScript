4f1956a6bd9fd1d687e8e202a8380f2efe8b44c9
Damian <damian14gil@gmail.com>
2022-10-07 17:35:23 +0200

    Ingresar numeros

---
+++ calculadora/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+alert("¡¡¡Bienvenido a la Calculadora!!!");
+let operacion = prompt("¿Qué operación desea realizar?: (+, -, * ó /)");
+let operadores = prompt("Por favor, introduzca dos operadores separados por espacio");
+switch(operacion) {
+    case '+': 
+    break;
+
+    case '-':
+    break;
+
+    case '*':
+    break;
+
+    case '/':
+    break;
+}
+
