4bf5bbf5d7161f04c62490fc5c26d8c9c0779eac
Daems033 <93617379+Daems033@users.noreply.github.com>
2022-12-07 02:25:23 +0100

    Modificación Ejercicio 2 Calculadora

--- Calculadora Funciones/calcPropio.js
+++ Calculadora Funciones/calcPropio.js
@@ -6,27 +6,33 @@ let op;
 let repetir = false;
 let operando1;
 let operando2;
+let operandos;
 
 function elegirOperador() {
   do {
     op = prompt('Elije operación: +, -, * ó /', '');
+    if (op === null) {
+      break;
+    }
     op = op.trim();
-    if(op !== '+' && op !== '-' && op !== '*' && op !== '/') {
+    if (op !== '+' && op !== '-' && op !== '*' && op !== '/') {
       alert('La operación introducida es errónea. Prueba de nuevo.');
     }
-  } while(op !== '+' && op !== '-' && op !== '*' && op !== '/');
+  } while (op !== '+' && op !== '-' && op !== '*' && op !== '/');
   return op;
 }
 
-let elegirOperandos = function() {
+let elegirOperandos = function () {
   operando1 = '';
   operando2 = '';
   let salir = false;
 
   while (!salir) {
-    let operandos = prompt('Introduce los operandos separados por espacio', '');
-
+    operandos = prompt('Introduce los operandos separados por espacio', '');
     let i = 0;
+    if (operandos === null) {
+      break;
+    }
     for (; i < operandos.length; i++) {
       if (operandos[i] !== ' ') {
         operando1 += operandos[i];
@@ -37,8 +43,6 @@ let elegirOperandos = function() {
       }
     }
 
-    console.log(operando1);
-
     for (; i < operandos.length; i++) {
       if (operandos[i] !== ' ') {
         operando2 += operandos[i];
@@ -48,7 +52,6 @@ let elegirOperandos = function() {
         }
       }
     }
-    console.log(operando2);
 
     operando1 = Number(operando1);
     operando2 = Number(operando2);
@@ -65,10 +68,16 @@ let elegirOperandos = function() {
 do {
 
   elegirOperador();
+  if (op === null) {
+    break;
+  }
   elegirOperandos();
+  if (operandos === null) {
+    break;
+  }
 
   switch (op) {
-    
+
     case '+':
       let suma = (operando1, operando2) => operando1 + operando2;
       alert("El resultado es: " + suma(operando1, operando2));
@@ -87,6 +96,4 @@ do {
       break;
   }
   repetir = confirm('Quieres realizar otra operación?');
-}while((op !== '+' && op !== '-' && op !== '*' && op !== '/') || repetir);
-
-console.log(op);
+} while ((op !== '+' && op !== '-' && op !== '*' && op !== '/') || repetir);
